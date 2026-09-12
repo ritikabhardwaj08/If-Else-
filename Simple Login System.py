043a17d90ccad0c645dbf9df9461ad9be6b52b07
ritikabhardwaj08 <ritikasharmabhardwaj1@gmail.com>
2026-09-12 10:42:27 +0530

    Add files via upload

---
+++ Simple Login System.py
@@ -0,0 +1,6 @@
+Username=input("Enter username: ")
+Password=input("Enter password: ")
+if Username=="admin" and Password=="1234":
+    print("Login successful")
+else:
+    print("Invalid username or password")
